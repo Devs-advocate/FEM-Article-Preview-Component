374a0354359b83ee5fafba36fce867fcb777bd57
Les <devles.proj@gmail.com>
2024-10-03 09:29:39 +1000

    First completion. Changed from img element of svg to svg code directly in HTML for the sharelink. Cleaned up commented code.

--- dist/script.js
+++ dist/script.js
@@ -1,21 +1,28 @@
-const shareBtn1 = document.querySelector('.personal .share-btn');
-const shareBtn2 = document.querySelector('.social-media-links .share-btn');
-const personal = document.querySelector('.personal');
+const shareSVG = document.querySelector('.share-btn svg path');
+const shareBtn = document.querySelector('.personal .share-btn');
 const links = document.querySelector('.social-media-links');
+
+shareSVG.setAttribute('fill', '#6E8098');
 
 if (links.style.display === 'none') {
   links.style.display = '';
   links.classList.add('off');
 }
 
-shareBtn1.addEventListener('click', toggleLinks);
-// shareBtn2.addEventListener('click', toggleLinks);
+shareBtn.addEventListener('click', toggleLinks);
 
-// function toggleLinks() {
-//   personal.classList.toggle('off');
-//   links.classList.toggle('off');
-// }
 function toggleLinks() {
-  // personal.classList.toggle('off');
   links.classList.toggle('off');
+
+  window.innerWidth > 849
+    ? links.classList.contains('off')
+      ? shareSVG.setAttribute('fill', '#6E8098')
+      : shareSVG.setAttribute('fill', 'white')
+    : '';
+
+  window.innerWidth > 849
+    ? links.classList.contains('off')
+      ? (shareBtn.style.backgroundColor = '')
+      : (shareBtn.style.backgroundColor = '#6E8098')
+    : '';
 }
